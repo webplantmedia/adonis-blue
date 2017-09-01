c94c8d7ff0603b47df79c054d11c87d089fee4cd
Chris Baldelomar <chris@webplantmedia.com>
2017-09-01 10:54:02 -0500

    set structure

--- footer.php
+++ footer.php
@@ -11,20 +11,23 @@
 
 ?>
 
+		</div><!-- .site-boundary -->
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'angiemakesdesign' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'angiemakesdesign' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'angiemakesdesign' ), 'angiemakesdesign', '<a href="http://angiemakes.com">Angie Makes</a>' );
-			?>
-		</div><!-- .site-info -->
+		<div class="site-boundary">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'angiemakesdesign' ) ); ?>"><?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'angiemakesdesign' ), 'WordPress' );
+				?></a>
+				<span class="sep"> | </span>
+				<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'angiemakesdesign' ), 'angiemakesdesign', '<a href="http://angiemakes.com">Angie Makes</a>' );
+				?>
+			</div><!-- .site-info -->
+		</div><!-- .site-boundary -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
