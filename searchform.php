10b4d01ed67078a43c4447b7d050933a78778a20
Chris Baldelomar <chris@webplantmedia.com>
2017-09-27 13:57:29 -0500

    update

---
+++ searchform.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Custom search form.
+ *
+ * @package AngieMakesDesign
+ */
+
+?>
+
+<div class="container">
+	<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<label>
+			<span class="screen-reader-text">
+				<?php /* Translators: this string is a label for a search input that is only visible to screen readers. */
+				esc_html_e( 'Search for:', 'angiemakesdesign' ); ?>
+			</span>
+
+			<input type="search" class="search-field" placeholder="<?php esc_html_e( 'Type keyword and hit enter', 'angiemakesdesign' ); ?>" title="<?php esc_attr_e( 'Press Enter to submit your search', 'angiemakesdesign' ) ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
+		</label>
+
+		<button type="submit" class="search-submit">
+			<i class="genericon genericon-search"></i>
+			<span class="screen-reader-text"><?php esc_html_e( 'Search', 'angiemakesdesign' ); ?></span>
+		</button>
+	</form>
+</div>
+
