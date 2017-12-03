9daae7d52d2cfaf2b2d972660c3113f6620aa29f
Chris Baldelomar <chris@webplantmedia.com>
2017-12-02 22:58:19 -0600

    1.8

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 /**
  * The current version of the theme.
  */
-define( 'ANGIE_MAKES_DESIGN_VERSION', '1.7' );
+define( 'ANGIE_MAKES_DESIGN_VERSION', '1.8' );
 
 if ( ! function_exists( 'angie_makes_design_setup' ) ) :
 	/**
