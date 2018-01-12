8e6b383746514dbeafd55f0b58bfe5601b230f55
Chris Baldelomar <chris@webplantmedia.com>
2018-01-11 23:10:44 -0600

    update

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 /**
  * The current version of the theme.
  */
-define( 'CRIMSON_ROSE_VERSION', '1.22' );
+define( 'CRIMSON_ROSE_VERSION', '1.23' );
 
 if ( ! function_exists( 'crimson_rose_setup' ) ) :
 	/**
