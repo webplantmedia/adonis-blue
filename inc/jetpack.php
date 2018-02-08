78e9aafd9a9b1a50e046d0330c97976dd0aa6e2d
Chris Baldelomar <chris@webplantmedia.com>
2018-02-08 09:37:00 -0600

    small style hacks

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -129,3 +129,10 @@ function crimson_rose_get_comment_display( $label ) {
 
 	return $html;
 }
+
+function crimson_rose_custom_thumb_size( $get_image_options ) {
+        $get_image_options['avatar_size'] = 128;
+ 
+        return $get_image_options;
+}
+add_filter( 'jetpack_top_posts_widget_image_options', 'crimson_rose_custom_thumb_size' );
