a3db953036ee959cf9a9b51bdece7aab046e6372
Chris Baldelomar <chris@webplantmedia.com>
2018-02-14 14:55:20 -0600

    BUG FIX: Firefox couldn't handle setPointerCapture. Disabled it.

--- inc/vendors/bxslider/js/jquery.bxslider.js
+++ inc/vendors/bxslider/js/jquery.bxslider.js
@@ -1106,10 +1106,10 @@
         slider.touch.start.x = touchPoints[0].pageX;
         slider.touch.start.y = touchPoints[0].pageY;
 
-        if (slider.viewport.get(0).setPointerCapture) {
-          slider.pointerId = orig.pointerId;
-          slider.viewport.get(0).setPointerCapture(slider.pointerId);
-        }
+        // if (slider.viewport.get(0).setPointerCapture) {
+          // slider.pointerId = orig.pointerId;
+          // slider.viewport.get(0).setPointerCapture(slider.pointerId);
+        // }
         // bind a "touchmove" event to the viewport
         slider.viewport.bind('touchmove MSPointerMove pointermove', onTouchMove);
         // bind a "touchend" event to the viewport
