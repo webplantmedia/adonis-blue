583db05ae5d48c891a12f9c5ebb672bda1f7d1be
Chris Baldelomar <chris@webplantmedia.com>
2018-05-07 03:04:44 -0500

    PICK: removed unused option site_structure

--- inc/default-options.php
+++ inc/default-options.php
@@ -39,7 +39,6 @@ $crimson_rose_default['disable_heading_font'] = 0;
 $crimson_rose_default['disable_accent_font'] = 0;
 $crimson_rose_default['page_image_header_height'] = 400;
 $crimson_rose_default['check_for_updates'] = 1;
-$crimson_rose_default['site_structure'] = 'box';
 
 // Blog
 $crimson_rose_default['blog_display'] = 'blog1';
