210c892894c0775019668fb329df006bba08fe87
Chris Baldelomar <chris@webplantmedia.com>
2018-05-08 05:42:42 -0500

    2.10

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 /**
  * The current version of the theme.
  */
-define( 'CRIMSON_ROSE_VERSION', '2.09' );
+define( 'CRIMSON_ROSE_VERSION', '2.10' );
 
 if ( ! function_exists( 'crimson_rose_setup' ) ) :
 	/**
--- readme.txt
+++ readme.txt
@@ -135,6 +135,6 @@ Source: https://www.pexels.com/photo/adult-cold-face-fashion-370013/
 
 == Changelog ==
 
-= 2.09 =
+= 2.10 =
 
 * Initial release
