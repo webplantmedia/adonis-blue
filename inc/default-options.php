e812f308ef57da6cd0d503945cf2cc3307e61ca8
Chris Baldelomar <chris@webplantmedia.com>
2018-07-26 18:30:46 -0500

    phpcbf

--- inc/default-options.php
+++ inc/default-options.php
@@ -69,7 +69,7 @@ $crimson_rose_default['blog_single_hide_post_navigation'] = 0;
 
 // Jetpack.
 $crimson_rose_default['jetpack_hide_share_count'] = 0;
-$crimson_rose_default['jetpack_scroll_credit'] = sprintf( esc_html__( 'Site crafted with', 'crimson-rose' ) . ' <i class="genericons-neue genericons-neue-heart"></i> ' . esc_html__( 'by', 'crimson-rose' ) . ' <a href="https://webplantmedia.com/">' . esc_html__( 'Web Plant Media', 'crimson-rose' ) . '</a>' );
+$crimson_rose_default['jetpack_scroll_credit']    = sprintf( esc_html__( 'Site crafted with', 'crimson-rose' ) . ' <i class="genericons-neue genericons-neue-heart"></i> ' . esc_html__( 'by', 'crimson-rose' ) . ' <a href="https://webplantmedia.com/">' . esc_html__( 'Web Plant Media', 'crimson-rose' ) . '</a>' );
 
 // Shop.
 $crimson_rose_default['shop_columns']                  = 2;
