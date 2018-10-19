1a5bdbaaa971ec573eb381c3d6fa70b0117da861
Chris Baldelomar <chris@webplantmedia.com>
2018-10-19 10:03:19 -0500

    PICK: CHECK: replaced menu location name

--- template-parts/menu-mobile.php
+++ template-parts/menu-mobile.php
@@ -11,7 +11,7 @@
  * @license http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-if ( ! has_nav_menu( 'menu-3' ) && ! has_nav_menu( 'menu-2' ) && ! has_nav_menu( 'jetpack-social-menu' ) ) {
+if ( ! has_nav_menu( 'menu-3' ) && has_nav_menu( 'social' ) ) {
 	return;
 }
 ?>
