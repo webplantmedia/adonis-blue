ceb39f04347197cba4ab305e217dd514971878ff
Chris Baldelomar <chris@webplantmedia.com>
2018-11-08 23:26:57 -0600

    1.27

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@
 /**
  * The current version of the theme.
  */
-define( 'PAINTED_LADY_VERSION', '1.26' );
+define( 'PAINTED_LADY_VERSION', '1.27' );
 
 if ( ! function_exists( 'painted_lady_setup' ) ) :
 	/**
