0ea07ce3a6f88c548e8e0528f6cfe07c8b36c4e6
Chris Baldelomar <chris@webplantmedia.com>
2018-11-15 08:46:43 -0600

    updated mobile menu

--- css/css-theme.php
+++ css/css-theme.php
@@ -219,7 +219,10 @@ a:not(.wp-block-button__link):hover {
 	background-color: ' . $painted_lady['archive_background_color'] . '; /*id:archive_background_color*/
 }
 
-.mobile-site-branding,
+.mobile-site-branding {
+	' . painted_lady_css_set_unit( 'padding-top', $painted_lady['heading_padding_bottom'] ) . ' /*id:heading_padding_bottom*/
+	' . painted_lady_css_set_unit( 'padding-bottom', $painted_lady['heading_padding_bottom'] ) . ' /*id:heading_padding_bottom*/
+}
 .site-branding {
 	' . painted_lady_css_set_unit( 'padding-top', $painted_lady['heading_padding_top'] ) . ' /*id:heading_padding_top*/
 	' . painted_lady_css_set_unit( 'padding-bottom', $painted_lady['heading_padding_bottom'] ) . ' /*id:heading_padding_bottom*/
